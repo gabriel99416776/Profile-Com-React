13a9fc1820f33a7addf5be3f973ebc77986658fb
gabriel99416776 <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>
2025-06-26 15:00:09 -0300

    tirado o props

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -2,21 +2,21 @@ import styles from "./styles.module.css"
 
 
 
+// (props) PODE SER SUBSTITUIDO PELO NOME DO ATRIBUTO.
 
-
-export default function Profile(props){
+export default function Profile({ name, avatar, bio, phone, email, githubUrl, linkedinUrl, instagramUrl }){
     return(
         <div className={styles.container}>
-            <img className={styles.avatar} src={props.avatar} alt={props.name} />
-            <h2>{props.name}</h2>
-            <div>{props.bio}</div>
-            <div>{props.phone}</div>
-            <div>{props.email}</div>
+            <img className={styles.avatar} src={avatar} alt={name} />
+            <h2>{name}</h2>
+            <div>{bio}</div>
+            <div>{phone}</div>
+            <div>{email}</div>
 
             <div className={styles.links}>
-                <a href={props.githubUrl} target="_blank">GitHub</a>
-                <a href={props.linkedinUrl} target="_blank">LikedIn</a>
-                <a href={props.instagramUrl} target="_blank">Instagram</a>
+                <a href={githubUrl} target="_blank">GitHub</a>
+                <a href={linkedinUrl} target="_blank">LikedIn</a>
+                <a href={instagramUrl} target="_blank">Instagram</a>
             </div>
         </div>
     )
